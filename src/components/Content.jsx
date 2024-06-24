c4bde5411ff6d5b5db81b0031cec6e86db493d2e
mine01 <patteera.banl@gmail.com>
2024-06-24 19:47:00 +0700

    update create project

---
+++ src/components/Content.jsx
@@ -0,0 +1,8 @@
+import '../css/Content.css'
+function Content() {
+  return (
+    <div className='container'>Content</div>
+  )
+}
+
+export default Content
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import '../css/Header.css'
+function Header() {
+  return (
+    <div className="container">Header</div>
+  )
+}
+
+export default Header;
